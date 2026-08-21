9890aaf0c3852e44b7e6e612f1894664af2d8573
Rosell Pretel <pretelrosell3@gmail.com>
2026-08-20 20:43:48 -0500

    Para el lab1, de Rosell

--- src/main/java/com/pucp/lab1gtics/Lab1GticsApplication.java
+++ src/main/java/com/pucp/lab1gtics/Lab1GticsApplication.java
@@ -9,7 +9,7 @@ public class Lab1GticsApplication {
 	public static void main(String[] args) {
 
 		System.out.println("Hola mundo");
-		System.out.println("Pruebaaaaaa");
+		System.out.println("Prueba de lab1");
 
 		SpringApplication.run(Lab1GticsApplication.class, args);
 	}
