cab2706663e5e205d4fd6708cacb34462b084896
Rosell Pretel <pretelrosell3@gmail.com>
2026-08-20 19:54:05 -0500

    Mi primer commit del lab1 2026-2

---
+++ src/main/java/com/pucp/lab1gtics/Lab1GticsApplication.java
@@ -0,0 +1,13 @@
+package com.pucp.lab1gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1GticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1GticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pucp/lab1gtics/Lab1GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucp.lab1gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1GticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
